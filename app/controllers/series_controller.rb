1ccabd4da020f70e363ebe48a89fa17a0c5c3755
jcartronics <jcar-95@hotmail.com>
2023-07-25 06:08:14 -0400

    Agrega metodo show

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -4,6 +4,11 @@ class SeriesController < ApplicationController
     @series = Serie.all
   end
 
+  # muestra una serie
+  def show
+    @serie = Serie.find(params[:id])
+  end
+
   # instancia una nueva serie
   def new
     @serie = Serie.new
@@ -11,19 +16,19 @@ class SeriesController < ApplicationController
 
   # crea una nueva serie
   def create
-    @serie = Serie.new(series_params)
+    @serie = Serie.new(serie_params)
 
     if @serie.save
-      redirect_to @serie
+      redirect_to series_path
     else
-      render "new"
+      render "new", status: :unprocessable_entity
     end
   end
 
   private
 
   # metodo para definir los parametros que se van a permitir
-  def series_params
+  def serie_params
     params.require(:serie).permit(:name, :synopsis, :director)
   end
 end
