675fd96aa7dd89f30af17a430448f370d14fd0f6
jcartronics <jcar-95@hotmail.com>
2023-07-25 06:07:34 -0400

    Modifica Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
-  # Rutas movies
-  get "/movies", to: "movies#index"
-  get "/movies/new", to: "movies#new"
-  post "/movies", to: "movies#create"
-  get "/movies/:id", to: "movies#show"
 
-  # Rutas documentary_films
-  get "/documentary_films", to: "documentary_films#index"
-  get "/documentary_films/new", to: "documentary_films#new"
-  post "/documentary_films", to: "documentary_films#create"
-  get "/documentary_films/:id", to: "documentary_films#show"
-
-  # Rutas series
   get "/series", to: "series#index"
   get "/series/new", to: "series#new"
-  post "/series", to: "series#create"
   get "/series/:id", to: "series#show"
+  post "/series", to: "series#create"
 
-  # root "articles#index"
+  get "/movies", to: "movies#index"
+  get "/movies/new", to: "movies#new"
+  get "/movies/:id", to: "movies#show"
+  post "/movies", to: "movies#create"
+
+  get "/documentary_films", to: "documentary_films#index"
+  get "/documentary_films/new", to: "documentary_films#new"
+  get "/documentary_films/:id", to: "documentary_films#show"
+  post "documentary_films", to: "documentary_films#create"
+
 end
+  # root "articles#index"
