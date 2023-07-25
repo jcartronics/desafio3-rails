c62ad47f6dc1c4da1dd142cab6c217a364f3bc01
jcartronics <jcar-95@hotmail.com>
2023-07-25 06:40:34 -0400

    Modifica rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
 
-  get "/series", to: "series#index"
-  get "/series/new", to: "series#new"
-  get "/series/:id", to: "series#show"
+  get "/series", to: "series#index", as: 'series'
+  get "/series/new", to: "series#new", as: 'new_serie'
   post "/series", to: "series#create"
+  get "/series/:id", to: "series#show", as: 'serie'
 
-  get "/movies", to: "movies#index"
-  get "/movies/new", to: "movies#new"
-  get "/movies/:id", to: "movies#show"
+  get "/movies", to: "movies#index", as: 'movies'
+  get "/movies/new", to: "movies#new", as: 'new_movie'
   post "/movies", to: "movies#create"
-
-  get "/documentary_films", to: "documentary_films#index"
-  get "/documentary_films/new", to: "documentary_films#new"
-  get "/documentary_films/:id", to: "documentary_films#show"
+  get "/movies/:id", to: "movies#show", as: 'movie'
+  
+  get "/documentary_films", to: "documentary_films#index", as: "documentary_films"
+  get "/documentary_films/new", to: "documentary_films#new", as: "new_documentary_film"
   post "documentary_films", to: "documentary_films#create"
+  get "/documentary_films/:id", to: "documentary_films#show", as: "documentary_film"
 
 end
   # root "articles#index"
