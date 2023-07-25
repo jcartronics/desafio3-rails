374aa5495c850b3d083d1847354c758089a8499b
jcartronics <jcar-95@hotmail.com>
2023-07-25 04:28:10 -0400

    Agrega logica al controller series

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,10 +1,29 @@
 class SeriesController < ApplicationController
-  def index
-  end
+# lista todas las series
+def index
+  @series = Series.all
+end
 
-  def create
-  end
+# crea una nueva serie
+def create
+  @series = Series.new(series_params)
 
-  def new
+  if @series.save
+    redirect_to @series
+  else
+    render 'new'
   end
+end
+
+# instancia una nueva serie
+def new
+  @series = Series.new
+end
+
+private
+
+# metodo para definir los parametros que se van a permitir
+def series_params
+  params.require(:series).permit(:name, :synopsis, :director)
+end
 end
