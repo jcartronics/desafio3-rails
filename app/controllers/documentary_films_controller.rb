4cc9562ceafff127fac3e346dc54af09f93161a5
jcartronics <jcar-95@hotmail.com>
2023-07-25 04:23:46 -0400

    Agrega logica al controller documentaryFilm

--- app/controllers/documentary_films_controller.rb
+++ app/controllers/documentary_films_controller.rb
@@ -1,10 +1,30 @@
 class DocumentaryFilmsController < ApplicationController
+
+  # lista todos los documentales
   def index
+    @documentary_films = DocumentaryFilm.all
   end
 
+  # crea un nuevo documental
   def create
+    @documentary_film = DocumentaryFilm.new(documentary_film_params)
+
+    if @documentary_film.save
+      redirect_to @documentary_film
+    else
+      render 'new'
+    end
   end
 
+  # instancia un nuevo documental
   def new
+    @documentary_film = DocumentaryFilm.new
+  end
+
+  private
+
+  # metodo para definir los parametros que se van a permitir
+  def documentary_film_params
+    params.require(:documentary_film).permit(:name, :synopsis, :director)
   end
 end
