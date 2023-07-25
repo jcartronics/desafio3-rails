796c9520243b2606d6a8801ead2a908f5af70378
jcartronics <jcar-95@hotmail.com>
2023-07-25 08:07:44 -0400

    Agrega seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do |i|
+    Movie.create(name: "Soy la pelicula #{i+1}", synopsis: "Soy la sinopsis #{i+1}", director: "Soy el director #{i+1}")
+    Serie.create(name: "Soy la serie #{i+1}", synopsis: "Soy la sinopsis #{i+1}", director: "Soy el director #{i+1}")
+    DocumentaryFilm.create(name: "Soy el documental #{i+1}", synopsis: "Soy la sinopsis #{i+1}", director: "Soy el director #{i+1}")
+  end
